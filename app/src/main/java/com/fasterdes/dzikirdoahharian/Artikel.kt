99200bdb2b7ac188f5bd792260d27af980b201ca
= <keyn432des@gmail.com>
2021-11-17 21:10:22 +0700

    New Project Nothing Happen

---
+++ app/src/main/java/com/fasterdes/dzikirdoahharian/Artikel.kt
@@ -0,0 +1,9 @@
+package com.fasterdes.dzikirdoahharian
+
+import java.io.FileDescriptor
+
+data class Artikel(
+    val imageArtikel: Int,
+    val titleArtikel: String,
+    val descriptor: String
+)
